62410f840c6f0a605556ed5f4509dcea317b1e8f
Almaz2077 <almaz.azhymatov@gmail.com>
2023-04-17 23:20:11 +0600

    initial commit

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = '5934219564:AAH8F42YYISlmMOM7YeMDFBRcg4F1tmiSZw'
+
+
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'password'
+DATABASE_HOST = 'localhost'
+DATABASE_NAME = 'car_db'
+
+# Ссылка на базу данных
+MYSQL_URL = f'mysql+pymysql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}'
